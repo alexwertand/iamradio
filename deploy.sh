c887ee3d8e7fbeb7964e24f3220490c3454c19f9
wert_and <alexandrphone07@gmail.com>
2022-09-29 05:26:35 +0300

    draw commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 set -e
 
+npm run build
+
 cd dist
 
 echo > .nojekyll
+
+git push -f git@github.com:alexwertand/iamradio.git main:gh-pages
 
 cd -
--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />fsdfsdfsdfv v fdvf d
+      <HelloWorld msg="You did it!" />fsdfsdfsdfv v fdvf d 1111111111111
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
